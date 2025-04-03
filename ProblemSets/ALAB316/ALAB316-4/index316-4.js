3e3650c9b4ad9b3dfecf427fdbabf103a25f8521
Rainikka <rainikka@gmail.com>
2025-04-02 23:24:51 -0400

    commit 2025-04-02 : 23:24

--- ProblemSets/ALAB316/ALAB316-4/index316-4.js
+++ ProblemSets/ALAB316/ALAB316-4/index316-4.js
@@ -29,60 +29,60 @@
 /**********************************************************/
 /************ Knowledge Inspiration: Geeks4Geeks **********/
 
-function formvalidate() {
-  /*** Get User Input Elements By ID ***/
-  const usernameInput = document.getElementById('username').value;
-  const emailInput = document.getElementById('email').value;
-  const passInput = document.getElementById('password').value;
-  const termsAgree = document.getElementById('terms').checked;
+const usernameErrorNote = "USERNAME ERROR: <br>Please select a username based on the following criteria: <br>The username cannot be blank.<br>The username must be at least four characters long.<br>The username must contain at least two unique characters.<br>The username cannot contain any special characters or whitespace.<br><br>";
 
-  /*** InnerHTML Error Messages By Type ***/
-  /*** Error Message Inner HTML ***/
-  const errorDisplay = document.getElementById('errorDisplay');
-  errorDisplay.id = 'errorDisplay';
+const emailErrorNote = "EMAIL ERROR: <br> Email must be typed in valid email formatd<br><br>";
 
-  const usernameError = errorDisplay.innerHTML = "";
-  const emailError = errorDisplay.innerHTML = "";
-  const passError = errorDisplay.innerHtml = "";
-  const termsError = errorDisplay.innerHHTML = "";
+const passErrorNote = "PASSWORD ERROR: <br>Passwords must be at least 12 characters long.<br> Passwords must have at least one uppercase and one lowercase letter.<br> Passwords must contain at least one number.<br> Passwords must contain at least one special character.<br> Passwords cannot contain the word 'password'.<br> Passwords cannot contain the username.<br> Both passwords must match.<br><br>";
 
-  /*** Get Error Message Elements By ID ***/
-  const btnRegister = document.getElementById("register");
+const termsErrorNote = "TERMS AGREEMENT ERROR: <br>Please agree to the terms of service.<br><br>";
 
-  /*** Username Regex Validation Requirements ***/
-  /*** Username inludes a letter and a number ***/
-  const nameAlphaNumeric = ([a - zA - Z0 - 9] + -);
-  /*** Username inludes 2 unique character ***/
-  const name2UniqueChars = ((?= (.* (.).*\2) { 1,}) - s);
-  /*** Email Regex Validation Requirements ***/
-  /*** Email inludes a "@" and "." ***/
-  const emailInvalid = (/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
-  /*** Password Regex Validation Requirements ***/
-  /*** Password inludes a "@" and "." ***/
-  const passInvalid = (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$ %^&*()\-_=+{};:,<.>]).{ 12,}$/);
+/**********************************************************/
+/************ Knowledge Inspiration: Geeks4Geeks **********/
+function formValidate(event) {
+  event.preventDefault(); {
+    /*** Get User Input Elements By ID ***/
+    const usernameInput = document.getElementById('username').value;
+    const emailInput = document.getElementById('email').value;
+    const passInput = document.getElementById('password').value;
+    const
+      passCheckInput = document.getElementById('passCheck').value;
+    const termsAgree = document.getElementById('terms').checked
 
-  /*** Validation Conditionals ****/
-  if (usernameInput === "") || (usernameInput.length < 4) || (!nameAlphaNumeric.test(usernameInput)) || (!name2UniqueChars.test(usernameInput)){
-    errorDisplay.innerHTML = usernameError;
-  } else if (emailInput === "") || (!emailInvalid.test(emailInput)) {
-    errorDisplay.innerHTML = emailError;
-  } else if (passInput === "") || (!passInvalid.test(passInput)) {
-    errorDisplay.innerHTML = passError;
-  } else {
-    console.log('Submit button clicked!');
+    /*** Get Error Display Message Element ***/
+    let errorDisplay = document.getElementById('errorDisplay');
+    errorDisplay.id = 'errorDisplay';
+    errorDisplay.style.display = 'block';
+    errorDisplay.innerHTML = '';
+
+    /*** Username Regex Validation Requirements ***/
+    /*** Username inludes a letter and a number ***/
+    const nameAlphaNumeric = /^[a-zA-Z0-9]+$/;
+    /*** Username inludes 2 unique character ***/
+    const name2UniqueChars = /^(?=.*(.).*\1)[a-zA-Z0-9]{4,}$/;
+    /*** Email Regex Validation Requirements ***/
+    /*** Email inludes a "@" and "." ***/
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    /*** Password Regex Validation Requirements ***/
+    /*** Password inludes a "@" and "." ***/
+    const passValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{ };:,<.>]).{12,}$/;
+
+    /*** Validation Conditionals ****/
+    if ((usernameInput === "") || (usernameInput.length < 4) || (!nameAlphaNumeric.test(usernameInput)) || (!name2UniqueChars.test(usernameInput))) {
+      errorDisplay.innerHTML = usernameErrorNote;
+    } else if ((emailInput === "") || (!emailValid.test(emailInput))) {
+      errorDisplay.innerHTML = emailErrorNote;
+    } else if ((passInput === "") || (passInput.length < 12) || (passInput !== passCheckInput) || (!passValid.test(passInput))) {
+      errorDisplay.innerHTML = passErrorNote;
+    } else if (!termsAgree.checked) {
+      errorDisplay.innerHTML = termsErrorNote;
+    } else {
+      errorDisplay.innerHTML = 'SUCCESS!<br><br>Your Registration Is Complete.';
+      console.log("success");
+    }
   }
 }
 
-const submitButton = document.querySelector('input[type="submit"]');
-
-// Add click event listener to the button submit Button
-addEventListener('click', function (event) {
-  console.log('Submit button clicked!');
-  // Your button click logic here
-});
-
-usernameError = "USERNAME ERROR: Please select a username based on the following criteria: <br>The username cannot be blank.<br>The username must be at least four characters long.<br>The username must contain at least two unique characters.<br>The username cannot contain any special characters or whitespace";
-
-emailError = "EMAILE ERROR: Email is not valid"";
-
-passError = "PASSWORD ERROR: Passwords must be at least 12 characters long.<br> Passwords must have at least one uppercase and one lowercase letter.<br> Passwords must contain at least one number.<br> Passwords must contain at least one special character.<br> Passwords cannot contain the word "password".<br> Passwords cannot contain the username.<br>Both passwords must match.";
+/*** Register Button Validation ***/
+const btnRegister = document.getElementById("register");
+btnRegister.addEventListener('click', formValidate);
