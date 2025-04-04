30fac92a53ffc7dd743bf314b8023438a323bcf8
Rainikka <rainikka@gmail.com>
2025-04-04 07:55:32 -0400

    innerHTML : select option

--- ProblemSets/ALAB4308-A/308A-4/index308A-4.js
+++ ProblemSets/ALAB4308-A/308A-4/index308A-4.js
@@ -18,4 +18,14 @@ async function start() {
 start();
 
 /*** Create HTML Selection Dropdown Tool */
-function createBreedList(breedList) { }
+function createBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+   <select>
+      <option>Choose A Breed</option>
+      <option>Boxer</option>
+      <option>Bulldog</option>
+      <option>Corgi</option>
+      <option>Pomeranian</option>
+      <option>St. Bernard</option>
+    </select>`
+}
