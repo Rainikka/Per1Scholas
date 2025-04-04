3e2ab87a2fa4de841e2310859c63ec9829b9a535
Rainikka <rainikka@gmail.com>
2025-04-04 07:43:31 -0400

    fetch breed list

--- ProblemSets/ALAB4308-A/308A-4/index308A-4.js
+++ ProblemSets/ALAB4308-A/308A-4/index308A-4.js
@@ -8,3 +8,14 @@
 /******* Objects & Arrays ********/
 /******** with JavaScript ********/
 /********** 04-APR-2025 **********/
+
+/*** Fetches the Dod CEO Data */
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all")
+  const data = await response.json()
+  createBBreedList(data.message)
+}
+start();
+
+/*** Create HTML Selection Dropdown Tool */
+function createBreedList(breedList) { }
