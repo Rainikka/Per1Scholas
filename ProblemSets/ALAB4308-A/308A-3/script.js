0469dfc37dcf97030c58bba17f7a49401f34ecbc
Rainikka <rainikka@gmail.com>
2025-04-09 12:41:16 -0400

    primary and foreign keys identified

--- ProblemSets/ALAB4308-A/308A-3/script.js
+++ ProblemSets/ALAB4308-A/308A-3/script.js
@@ -1,16 +1,63 @@
-/*********************************/
-/******* Rainikka Corprew ********/
-/********** JAVASCRIPT ***********/
-/************ 308A.3 *************/
-/*********************************/
-
 /**********************************/
-/* API Fetch, Axios, Async, Await */
-/******** with JavaScript ********/
-/********** 07-APR-2025 **********/
+/******** Rainikka Corprew ********/
+/*********** JAVASCRIPT ***********/
+/************ 308A.3 **************/
+/**** Promises, Async & Await *****/
+/********** 09-APR-2025 ***********/
+/**********************************/
 
-/********** FETCHING AN API *********/
+/******************* LAB DESCRIPTION **********************
+ * Here's the scenario: you are a developer in a very large 
+ * corporation that splits its data across multiple databases.
+ * Your job is to assemble this information using a single 
+ * function that takes an id parameter and returns Promise 
+ * that resolves to an object containing specific data.
+ * The object must contain the following information, which 
+ * will be gathered from the databases:
+ * 
+{
+  id: number,
+    name: string,
+      username: string,
+        email: string,
+          address: {
+    street: string,
+      suite: string,
+        city: string,
+          zipcode: string,
+            geo: {
+      lat: string,
+        lng: string
+    }
+  },
+  phone: string,
+    website: string,
+      company: {
+    name: string,
+      catchPhrase: string,
+        bs: string
+  }
+}
+**********************************************/
+/********** Promises, Async & Await  *********/
 
+/*************** Databse Relational Structure ****************/
+/***** Primary Key: id, Source: central db, Type: number *****
+* db1: id: 1-4
+* db2: id: 4 - 7
+* db3: id: 8 - 9 
+*
+/***** Foreign Keys: Data Source, Type, & Order ******
+* 0. central db.id
+* 1. db.username: string
+* 2. vt.name: string
+* 3. vt.email: string
+*4. vt.address: obj 
+* 5. vt.phone: string
+* 6. db.website,: string
+* 7. db.company. obj
+/**************************************************************/
+  
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
