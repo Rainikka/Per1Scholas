c19210921f4708afa4320f47ec0f728a8fd9fb84
Rainikka <rainikka@gmail.com>
2025-04-09 11:23:20 -0400

    files copied froom codeSandbox

--- ProblemSets/ALAB4308-A/308A-3/script.js
+++ ProblemSets/ALAB4308-A/308A-3/script.js
@@ -11,4 +11,14 @@
 
 /********** FETCHING AN API *********/
 
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+}
 
