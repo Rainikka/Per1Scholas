c004e3cd8a6b156180c4081a163a00e561c4fa24
Rainikka <rainikka@gmail.com>
2025-04-12 15:24:20 -0400

    corrected import code

--- ProblemSets/crm/index.js
+++ ProblemSets/crm/index.js
@@ -1,12 +1,12 @@
-impoort express from 'express';
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) =>
-  res.send(`Node and express server is running on por ${PORT}`)
-);
+app.get('/', (req, res) => {
+  res.send(`Node and express server is running on port ${PORT}`);
+});
 
-app.listen(PORT, () =>
-  console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+});
