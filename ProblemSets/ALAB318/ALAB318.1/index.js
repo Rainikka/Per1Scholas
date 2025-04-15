475665ae91d0d9e4ae84a478fae23671234b16fb
Rainikka <rainikka@gmail.com>
2025-04-15 10:58:59 -0400

    npm i

---
+++ ProblemSets/ALAB318/ALAB318.1/index.js
@@ -0,0 +1,27 @@
+
+/********************* PART 1 *********************/
+// node.js installed successfully
+
+/********************* PART 2 *********************/
+/**** TASK 1: JavaScript code from the command line using the “eval” and “print” commands.The --eval or -e flag tells Node to evaluate the following string as a JavaScript expression.The--print or -p flag behaves similarly, but automatically prints the result if possible. ***/
+
+/** Requested operation entered on command line with Node  eval flag as method for displaying answered response  ***
+ * 
+ * Requested Operation: node -e 'console.log(17 + 25)' 
+ * Response Answer: 42 
+ *
+/** Requested operation entered on command line with Node  eval flag as method for displaying answered response  ***
+ * Requested Operation: node -p '17 + 25'
+ * Response Answer: 42 
+ ****
+ 
+ /**** TASK 2: Inside of the new JavaScript file, write some arbitrary code.We will use the classic “Hello World” example. Ensure that your file has some type of output using console.log! ***/
+console.log('Hello World');
+
+/********************* PART 3 *********************/
+/** Generate default npm pakage
+ * Open the package.json file.
+ * Within “scripts” object, add a new entry under “test” called “start.” Set the value of “start” to “node index.js.” **/
+
+
+// npm i --save-dev nodemon
