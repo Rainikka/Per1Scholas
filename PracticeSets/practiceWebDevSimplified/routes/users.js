9a149725219eddd8335589388c27c35ffe737605
Rainikka <rainikka@gmail.com>
2025-04-16 04:56:29 -0400

    /*** Update User By ID ***/

--- PracticeSets/practiceWebDevSimplified/routes/users.js
+++ PracticeSets/practiceWebDevSimplified/routes/users.js
@@ -5,17 +5,27 @@ const router = express.Router()
 
 /*** Set-Up: General User Router ***/
 router.get('/', (req, res) => {
-  res.send('User List')
+  res.send('User ToDo List')
 })
 
 /*** Set-Up: New User Router ***/
 router.get('/new', (req, res) => {
-  res.send('User New Form')
+  res.send('User New ToDo Form')
 })
 
 /*** Set-Up: Create New User Router ***/
 router.post('/', (req, res) => {
-  res.send('Create New User')
+  res.send('Create New ToDo User')
+})
+
+/*** Get User By ID ***/
+router.get('/:userId', (req, res) => {
+  res.send(`Get User with ID ${req.params.userId} `)
+})
+
+/*** Update User By ID ***/
+router.put('/:userId', (req, res) => {
+  res.send(`Update User with ID ${req.params.userId} `)
 })
 
 /*** Get User By ID ***/
