e6c9accfca9ae0fc8b7233fb38f5896bc5c6d849
Rainikka <rainikka@gmail.com>
2025-04-16 05:38:10 -0400

    updating code

--- PracticeSets/practiceWebDevSimplified/routes/posts.js
+++ PracticeSets/practiceWebDevSimplified/routes/posts.js
@@ -8,27 +8,27 @@ router.get('/', (req, res) => {
   res.send('Get ToDo List')
 })
 
-/*** Set-Up: New ToDo List Router ***/
+/*** Set-Up: New ToDo2TaDa List Router ***/
 router.get('/new', (req, res) => {
   res.send('New ToDo Item')
 })
 
-/*** Set-Up: Create New Task Line Item Router ***/
+/*** Set-Up: Create New ToDo2TaDa Task Line Item Router ***/
 router.post('/', (req, res) => {
   res.send('Create New ToDo List Item')
 })
 
-/*** Get Task Line Item By ID ***/
+/*** Get ToDo2TaDa Task Line Item By ID ***/
 router.get('/:userId', (req, res) => {
   res.send(`Get User with ID ${req.params.userId} `)
 })
 
-/*** Update Task Line Item By ID ***/
+/*** Update ToDo2TaDa Task Line Item By ID ***/
 router.put('/:userId', (req, res) => {
   res.send(`Update User with ID ${req.params.userId} `)
 })
 
-/*** Delete Task Line Item By ID ***/
+/*** Delete ToDo2TaDa Task Line Item By ID ***/
 router.delete('/:userId', (req, res) => {
   res.send(`Delete User with ID ${req.params.userId} `)
 })
--- PracticeSets/practiceWebDevSimplified/routes/users.js
+++ PracticeSets/practiceWebDevSimplified/routes/users.js
@@ -1,36 +1,41 @@
 
-/*** Set-Up: User Router ***/
+/*** Set-Up: ToDo2TaDa User Router ***/
 const express = require('express')
 const router = express.Router()
 
-/*** Set-Up: General User Router ***/
+/*** Set-Up: General ToDo2TaDa User Router ***/
 router.get('/', (req, res) => {
   res.send('Get ToDo User')
 })
 
-/*** Set-Up: New User Router ***/
+/*** Set-Up: New ToDo2TaDa User Router ***/
 router.get('/new', (req, res) => {
   res.send('User New ToDo Form')
 })
 
-/*** Set-Up: Create New User Router ***/
+/*** Set-Up: Create New ToDo2TaDa User Router ***/
 router.post('/', (req, res) => {
   res.send('Create New ToDo User')
 })
 
-/*** Get User By ID ***/
+/*** Get ToDo2TaDa User By ID ***/
 router.get('/:userId', (req, res) => {
   res.send(`Get User with ID ${req.params.userId} `)
 })
 
-/*** Update User By ID ***/
+/*** Update ToDo2TaDa User By ID ***/
 router.put('/:userId', (req, res) => {
   res.send(`Update User with ID ${req.params.userId} `)
 })
 
-/*** Delete User By ID ***/
+/*** Delete ToDo2TaDa User By ID ***/
 router.delete('/:userId', (req, res) => {
   res.send(`Delete User with ID ${req.params.userId} `)
+})
+
+
+router.param("id", (req, res, next, id) => {
+  console.log(id)
 })
 
 /*** Export User Router ***/
