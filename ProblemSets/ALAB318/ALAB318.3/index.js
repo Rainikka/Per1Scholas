cb074c2be6c7d8e04077e1d51ab6d4a52dc41db4
Rainikka <rainikka@gmail.com>
2025-04-18 04:36:18 -0400

    added app.use(express.urlencoded({ extended: true })); app.use(express.json());

--- ProblemSets/ALAB318/ALAB318.3/index.js
+++ ProblemSets/ALAB318/ALAB318.3/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const users = require("./routes/users");
 const posts = require("./routes/posts");
