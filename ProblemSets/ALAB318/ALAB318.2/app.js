265d8ff2290bed50f151c936b7513c57afb2c024
Rainikka <rainikka@gmail.com>
2025-04-18 12:37:16 -0400

    /*** Set-Up: Express Server Module ***/

--- ProblemSets/ALAB318/ALAB318.2/app.js
+++ ProblemSets/ALAB318/ALAB318.2/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+/******************************/
+/****** Rainikka Corprew ******/
+/********* JAVASCRIPT *********/
+/******** ALAB 318.2 **********/
+/******************************/
+
+/******************************/
+/***** BUILDING AN EXPRESS ****/
+/******** APPLICATION *********/
+/******** 18-APR-2025 *********/
+
+
+/*** Set-Up: Express Server Module ***/
+const express = require("express")
+const app = express()
+
+
