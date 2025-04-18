80ff0e1943f1cc202f168da4f8bc58c08d5f943c
Rainikka <rainikka@gmail.com>
2025-04-18 03:46:50 -0400

    npm init to files moved from codeSandbox

---
+++ ProblemSets/ALAB318/ALAB318.3/server.js
@@ -0,0 +1,33 @@
+
+/******************************/
+/****** Rainikka Corprew ******/
+/********* JAVASCRIPT *********/
+/******** ALAB 318.3 **********/
+/******************************/
+
+/******************************/
+/******** RESTFUL API *********/
+/********* EXPANSION **********/
+/******** 18-APR-2025 *********/
+
+/************ PRE-CODE SET-UP & REQUIREMENTS ************/
+/*******************************************************/
+
+/****** GIT & NODE INITIALIZATIONS *******
+ * git init  ~~ git add . ~~  git commit -m "note" ~~ git push
+ * npm init -y
+ * npm i express body-parser method-override ejs dotenv
+ * npm i --save-dev nodemon
+ 
+ * In package.json file, 
+ * replace this key/value pair:
+    * "scripts":  
+       -"test": "echo\"Error: no test specified\" && exit 1" 
+  
+  * with this key/value pair ******
+    * "scripts": {
+        - "start": "node server.js",
+        - "devStart": "nodemon server.js"
+   * },
+   
+**** nodemon devStart ~~ when ready to run server: automatically restarts server anytime there are changes to code. ******
