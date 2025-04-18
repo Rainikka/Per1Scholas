dd57c8271a570edaffbccd9b2ac11acd66d93a80
Rainikka <rainikka@gmail.com>
2025-04-18 13:23:21 -0400

    /** Set-Up: Port Listening Function ***/

--- ProblemSets/ALAB318/ALAB318.2/app.js
+++ ProblemSets/ALAB318/ALAB318.2/app.js
@@ -11,7 +11,23 @@
 
 
 /*** Set-Up: Express Server Module ***/
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
 
+/*** Set-Up: API Endpoint Routes ***/
+
+/** Get Route: Home Page ***/
+app.get('/', (req, res) => {
+  res.send('Hello World')
+
+});
+
+
+
+
+/** Set-Up: Port Listening Function ***/
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
