c189b11abfdf4729cde1ea746e6ca68ebad6e333
Rainikka <rainikka@gmail.com>
2025-04-18 14:08:19 -0400

    aet-up views: pug template engine

--- ProblemSets/ALAB318/ALAB318.2/app.js
+++ ProblemSets/ALAB318/ALAB318.2/app.js
@@ -9,11 +9,9 @@
 /******** APPLICATION *********/
 /******** 18-APR-2025 *********/
 
-
 /*** Set-Up: Express Server Module ***/
 const express = require("express");
 const app = express();
-const path - require('path')
 const PORT = 3000;
 
 /*** Set-Up Views: Template Engine ***/
