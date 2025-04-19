4aef75f6ee2f430a315d7de66cb9627a5380b1a4
Rainikka <rainikka@gmail.com>
2025-04-18 22:41:13 -0400

    /**** refactoring code ***/

--- ProblemSets/ALAB318/ALAB318.2/app.js
+++ ProblemSets/ALAB318/ALAB318.2/app.js
@@ -9,59 +9,111 @@
 /******* APPLICATION *********/
 /******* 18-APR-2025 *********/
 
+/******** Knowledge Inspiration 1 ********
+ * 
+ * Media Outlet :: YouTube 
+ * Title :: Node.js & Express 
+ * Creator/Autor :: Traversy Media
+ * 
+ ******** Knowledge Inspiration 2 ********
+ * 
+ * Media Outlet:: Geeks4Geeks
+ * Title :: Pug View Engne Installation
+ * Creator / Autor :: Geeks4Geeks
+ * 
+ * ******** Knowledge Inspiration 3 ********
+ * 
+ * Media Outlet :: Medium.com
+ * Title :: Hosting a JSON API on GitHub Pages
+ * Creator / Autor :: Vistor Scholz
+ * 
+ * *****************************************
 
+
+/*********** INSTRUCTIONS **************
+ * This lab is intended to give you time
+ * to practice what you learned, while
+ * also researching and implementing new
+ * concepts.
+ * 
+ * Objectives
+ * 
+ * Create an Express application
+ * Implement an Express template engine
+ * Implement routes with route parameters
+ * Render template views.
+ * Implement Express middleware.
+ * erve static files.
+ * end files for download.
+ * 
+ *****************************************
+
+
+/******** Set-Up: Express Server ********/
 /*** Set-Up: Express Server Module ***/
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
-/*** Middleware: Parse Form Data ***/
+/*** Set-Up Views: Template Engine ***/
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+/*** Set-Up: Middleware ***/
 app.use(express.urlencoded({ extended: true }));
 
-/*** Set-Up Views: Template Engine ***/
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-/*** Sample Data: In-Memory Storage ***/
+/*** Set-Up: Array of Article ***/
 let articles = [
-  { id: 1, title: "Article One", author: "Rain Corp", body: "This is article one" },
-  { id: 2, title: "Article Two", author: "Rain Corp", body: "This is article two" },
-  { id: 3, title: "Article Three", author: "Rain Corp", body: "This is article three" }
+  {
+    id: 1,
+    title: 'Article One',
+    author: 'Rain Corp',
+    body: 'This is article one'
+  },
+  {
+    id: 2,
+    title: 'Article Two',
+    author: 'Rain Corp',
+    body: 'This is article two'
+  },
+  {
+    id: 3,
+    title: 'Article Three',
+    author: 'Rain Corp',
+    body: 'This is article three'
+  }
 ];
 
-/*** Set-Up: API Endpoint Routes ***/
-
-/** Route: Homepage (Render Articles) ***/
+/** Set-Up View: Render Home Page ***/
 app.get('/', (req, res) => {
   res.render('index', {
     title: "Articles",
-    articles: articles  // Fixed: Now passes `articles` to the template
+    articles: articles
   });
 });
 
-/** GET Route: Add Article ***/
-app.get('/articles/add', (req, res) => {
-  res.render('add_article', {
-    title: 'Add Article'
-  });
+/** Set-Up View: About Page ***/
+app.get('/about', (req, res) => {
+  res.render('about', { title: "About" });
 });
 
-/** POST Route: Handle Form Submission (POST) ***/
-app.post('/articles/add', (req, res) => {
+/** Set-Up View: Form to Add Articles ***/
+app.get('/add', (req, res) => {
+  res.render('add', { title: "Add Article" });
+});
+
+app.post('/add', (req, res) => {
   const { title, author, body } = req.body;
-  const newArticle = { id: articles.length + 1, title, author, body };
+  const newArticle = {
+    id: articles.length + 1,
+    title,
+    author: " "
+  };
   articles.push(newArticle);
   res.redirect('/');
 });
 
-/** Route: About Page ***/
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About'
-  });
-});
-
-/** Start Server: Port Listening Function ***/
+/** Set-Up:  Port to Run the Server ***/
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
