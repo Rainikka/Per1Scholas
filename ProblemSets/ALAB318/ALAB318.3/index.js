dd5cfa2ab6aa31a77ad0fd63139efa5344b02098
Rainikka <rainikka@gmail.com>
2025-04-20 12:06:40 -0400

    functioning: get all users and posts

--- ProblemSets/ALAB318/ALAB318.3/index.js
+++ ProblemSets/ALAB318/ALAB318.3/index.js
@@ -8,7 +8,7 @@
 /******************************/
 /******** RESTFUL API *********/
 /********* EXPANSION **********/
-/******** 18-APR-2025 *********/
+/******** 20-APR-2025 *********/
 
 /*** Set-Up: Express Server ***/
 const express = require("express");
