86167898e1d8766e1d026217244e5cf65d1b5ec8
Rainikka <rainikka@gmail.com>
2025-04-20 12:05:00 -0400

    get posts and users work

--- ProblemSets/ALAB318/ALAB318.3/index.js
+++ ProblemSets/ALAB318/ALAB318.3/index.js
@@ -117,46 +117,65 @@ app.get("/api", (req, res) => {
 
 /*** GET / api / users /: id / posts ***/
 // Retrieves all posts by a user with the specified id.
-// GET /api/users/:id/posts - Get all posts by a specific user
-// GET /api/users/:id/posts - Get all posts by a specific user
-app.get("api/:userid/posts", (req, res, next) => {
+app.get("/api/users/:id/posts", (req, res, next) => {
   const userId = parseInt(req.params.id);
+  res.json({ message: `Posts for user ${userId} would go here` });
 });
 
-/***  GET / api / posts ? userId = <VALUE> ***/
+/*** GET / api / posts ? userId = <VALUE> ***/
 // Retrieves all posts by a user with the specified postId.
+app.get("/api/posts", (req, res) => {  // Added basic implementation
+  if (req.query.userId) {
+    return res.json({ message: `Posts for user ${req.query.userId} would go here` });
+  }
+  res.json({ message: "All posts would go here" });
+});
 
-//   GET /comments
-//   Note that we do not have any comments data yet; that is okay! Make sure that you create a
-//   place to store comments, but you do not need to populate that data.
-//   POST /comments
-//   When creating a new comment object, it should have the following fields: id: a unique identifier.
+/*** GET / api / comments ***/
+// Place to store comments, but you do not need to populate that data.
+app.get("/api/comments", (req, res) => {  // Added basic implementation
+  res.json({ message: "Comments would go here" });
+});
+
+/*** POST / api / comments ***/
+// Create a new comment object, with fields: id: a unique identifier.
 //   userId: the id of the user that created the comment.
 //   postId: the id of the post the comment was made on.
-//   body: the text of the comment. GET /comments/:id
-//   Retrieves the comment with the specified id.
-//   PATCH /comments/:id
-//   Used to update a comment with the specified id with a new body.
-//   DELETE /comments/:id
-//   Used to delete a comment with the specified id.
-//   GET /comments?userId=<VALUE>
-//     Retrieves comments by the user with the specified userId.
-//     GET /comments?postId=<VALUE>
-//       Retrieves comments made on the post with the specified postId.
-//       GET /posts/:id/comments
-//       Retrieves all comments made on the post with the specified id.
-//       GET /users/:id/comments
-//       Retrieves comments made by the user with the specified id.
-//       GET /posts/:id/comments?userId=<VALUE>
-//         Retrieves all comments made on the post with the specified id by a user with the specified
-//         userId.
-//         GET /users/:id/comments?postId=<VALUE>
-//           Retrieves comments made by the user with the specified id on the post with the specified postId.
+//   body: the text of the comment.
+app.post("/api/comments", (req, res) => {
+  res.json({ message: "Comment would be created here" });
+});
+
+/*** GET / api / comments / :id ***/
+// Retrieves the comment with the specified id.
+app.get("/api/comments/:id", (req, res) => {
+  res.json({ message: `Comment ${req.params.id} would go here` });
+});
+
+/*** PATCH / api / comments / :id ***/
+// Used to update a comment with the specified id with a new body.
+app.patch("/api/comments/:id", (req, res) => {  /
+  res.json({ message: `Comment ${req.params.id} would be updated here` });
+});
+
+/*** DELETE / api / comments / :id ***/
+// Used to delete a comment with the specified id.
+app.delete("/api/comments/:id", (req, res) => {  // Added basic implementation
+  res.json({ message: `Comment ${req.params.id} would be deleted here` });
+});
+
+/***  GET / api / comments?userId=<VALUE>
+// Retrieves comments by the user with the specified userId.
+
+/***  GET / api / comments?postId=<VALUE>
+// Retrieves comments made on the post with the specified postId.
+
 
 // 404 Middleware
 app.use((req, res, next) => {
   next(error(404, "Resource Not Found"));
 });
+
 // Error-handling middleware.
 // Any call to next() that includes an
 // Error() will skip regular middleware and
