1bdb0857689b0e08a11d558597eafb32f4d50f19
Rainikka <rainikka@gmail.com>
2025-04-19 20:53:54 -0400

    commit to commit

--- ProblemSets/ALAB318/ALAB318.3/index.js
+++ ProblemSets/ALAB318/ALAB318.3/index.js
@@ -10,6 +10,7 @@
 /********* EXPANSION **********/
 /******** 18-APR-2025 *********/
 
+
 /*** Set-Up: Express Server ***/
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -133,6 +134,48 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({ error: err.message });
 });
+
+/********************* START OF ASSIGNMENT **********************/
+/** Create the following routes using good organizational coding **/
+
+/*** GET / api / users /: id / posts ***/
+// Retrieves all posts by a user with the specified id.
+// GET /api/users/:id/posts - Get all posts by a specific user
+// GET /api/users/:id/posts - Get all posts by a specific user
+app.get("api/:userid/posts", (req, res, next) => {
+  const userId = parseInt(req.params.id);
+});
+
+/***  GET / api / posts ? userId = <VALUE> ***/
+// Retrieves all posts by a user with the specified postId.
+
+
+//   GET /comments
+//   Note that we do not have any comments data yet; that is okay! Make sure that you create a
+//   place to store comments, but you do not need to populate that data.
+//   POST /comments
+//   When creating a new comment object, it should have the following fields: id: a unique identifier.
+//   userId: the id of the user that created the comment.
+//   postId: the id of the post the comment was made on.
+//   body: the text of the comment. GET /comments/:id
+//   Retrieves the comment with the specified id.
+//   PATCH /comments/:id
+//   Used to update a comment with the specified id with a new body.
+//   DELETE /comments/:id
+//   Used to delete a comment with the specified id.
+//   GET /comments?userId=<VALUE>
+//     Retrieves comments by the user with the specified userId.
+//     GET /comments?postId=<VALUE>
+//       Retrieves comments made on the post with the specified postId.
+//       GET /posts/:id/comments
+//       Retrieves all comments made on the post with the specified id.
+//       GET /users/:id/comments
+//       Retrieves comments made by the user with the specified id.
+//       GET /posts/:id/comments?userId=<VALUE>
+//         Retrieves all comments made on the post with the specified id by a user with the specified
+//         userId.
+//         GET /users/:id/comments?postId=<VALUE>
+//           Retrieves comments made by the user with the specified id on the post with the specified postId.
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
