035431f8118200cfe7fe5569751ecd6c1ba34a7d
Rainikka <rainikka@gmail.com>
2025-04-24 01:20:13 -0400

    Cluster connection

--- ProblemSets/ALAB319/FruitDB/server.js
+++ ProblemSets/ALAB319/FruitDB/server.js
@@ -32,9 +32,34 @@ mongoose.connect(process.env.ATLAS_URI)
 mongoose.connection.once('open', () => {
   console.log('connected to mongoDB')
 })
+app.get('/fruits/new', (req, res) => {
+  res.render('fruits/New');
+});
+
+app.post('/fruits/', (req, res) => {
+  res.send('received');
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/fruits/', (req, res) => {
+  res.send(req.body);
+});
+
+app.post('/fruits/', (req, res) => {
+  if (req.body.readyToEat === 'on') { //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true;
+  } else { //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false;
+  }
+  fruits.push(req.body);
+  res.redirect('/fruits')
+});
 
 
-console.log(process.env.MONGO_URI);
+
 app.listen(3000, () => {
   console.log(`Server is listeing on ${PORT}`)
-})
+});
+
+
