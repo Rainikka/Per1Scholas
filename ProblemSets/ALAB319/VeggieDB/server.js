58d81932bd0299a2523a22e1166223f2428f88bc
Rainikka <rainikka@gmail.com>
2025-04-25 10:42:09 -0400

    /*** Set-Up Expres Server ***/

--- ProblemSets/ALAB319/VeggieDB/server.js
+++ ProblemSets/ALAB319/VeggieDB/server.js
@@ -1,3 +1,10 @@
 /*** Secured Connection String ***/
 const dotenv = reuiqre("dotenv");
 dotenv.config();
+
+/*** Set-Up Expres Server ***/
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const mongoose = require("mongoose")
