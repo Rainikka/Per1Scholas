22132c432878fda77afd431064d0caba5002ffec
Rainikka <rainikka@gmail.com>
2025-04-27 15:15:14 -0400

    /*** server listening ***/

---
+++ ProblemSets/ALAB319/SBA319/server.js
@@ -0,0 +1,21 @@
+/*** Secured connection string ***/
+const dotenv = require('dotenv').config
+
+/*** Start Server ***/
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+/*** Addition: Middleware ***/
+app.use(express.json());
+
+/** Mongoose ***/
+
+
+
+
+/*** Server Listening***/
+app.listen(3000, () => {
+  console.log(`Server listing at port: André${PORT}`)
+
+});
