c0ed6f3a46729d4318deead89c3e5955271a8794
Rainikka <rainikka@gmail.com>
2025-04-28 04:54:09 -0400

    cloning problemset folders to projects folder

--- ProjectPages/alab319-5/routes/Fruits.js
+++ ProjectPages/alab319-5/routes/Fruits.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Veggie = require('../models/Veggie');
+const Fruit = require('../models/Fruit');
 
 router.get('/', async (req, res) => {
   try {
-    const veggies = await Veggie.find();
-    res.json(veggies);
+    const fruits = await Fruit.find();
+    res.json(fruits);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
@@ -14,23 +14,24 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const foundVeggie = await Veggie.findById(req.params.id);
-    res.json(foundVeggie);
+    const foundFruit = await Fruit.findById(req.params.id);
+    res.json(foundFruit);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
   }
 });
 
+// Create Route
 router.post('/', async (req, res) => {
-  const veggie = new Veggie({
+  const fruit = new Veggie({
     name: req.body.name,
     color: req.body.color,
     readyToEat: req.body.readyToEat
   });
 
-  const newVeggie = await veggie.save();
-  res.status(201).json(newVeggie);
+  const newFruit = await fruit.save();
+  res.status(201).json(newFruit);
 });
 
 module.exports = router;
--- ProjectPages/alab319-5/server.js
+++ ProjectPages/alab319-5/server.js
@@ -4,9 +4,24 @@
 /******************************/
 
 /******************************/
-/****** WORKING WITH  ********/
-/******** MONGOOSE *********/
+/******* WORKING WITH *********/
+/********** MONGOOSE **********/
 /******** 25-APR-2025 *********/
+
+/******** Knowledge Inspiration 1 ********
+ * 
+ * Media Outlet :: YouTube
+ * Title :: Learn MongoDBb
+ * Title :: Learn Mongoose
+ * Creator/Author :: WebDevSimplified
+ * 
+******** Knowledge Inspiration 2 ********
+ * 
+ * Media Outlet :: FreeCodeCamp
+ *  Title :: How to Build a RESTful API
+ *  Creator / Author :: Nishant Kumar
+ *  
+ *****************************************/
 
 /*********** ENVIRONMENT SET-UP ***********/
 /******************************************/
@@ -18,9 +33,10 @@ dotenv.config();
 /*** Require: Express Server ***/
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 /*** Require: Middleware ***/
+const urlencode = require("express");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -29,11 +45,15 @@ const mongoDB = require("mongodb");
 const mongoose = require("mongoose");
 
 /*** Require: Database Models ***/
+const Fruit = require('./models/Fruit.js');
+const Fruits = require('./routes/Fruits.js');
+
 const Veggie = require('./models/Veggie.js');
 const Veggies = require('./routes/Veggies.js');
 
+
 /*** Require: Database Connection ***/
-mongoose.connect(process.env.ATLAS_URI)
+mongoose.connect(process.env.DATABASE_URI)
 mongoose.connection.once('open', () => {
   console.log('Connected to mongoDB')
 });
@@ -42,11 +62,38 @@ mongoose.connection.once('open', () => {
 /*************** INDUCES **************/
 /*** Index: Get ***/
 app.get('/', (req, res) => {
-  res.send(`Welcome to the Veggie API!
-    Go ahead and introduce yourself to a veggie today`)
+  res.send(`Welcome to the Fruit & Veggie API!
+    Go ahead and re-introduce yourself to a produce--put a fresh fruit or vegetable  on your plate today!`)
 })
 
-/***  Seed Data Function ***/
+/*** Seed Data Function :: Fruit ***/
+app.get('/frutis/seed', async (req, res) => {
+  try {
+    await Fruit.create([
+      {
+        name: 'banana',
+        color: 'yellow',
+        readyToEat: true
+      },
+      {
+        name: 'apple',
+        color: 'spotted',
+        readyToEat: false
+      },
+      {
+        name: 'canteloupe',
+        color: 'orange',
+        readyToEat: true
+      }
+    ])
+    res.redirect('/fruits')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Error seeding data")
+  }
+});
+
+/*** Seed Data Function :: Veggie ***/
 app.get('/veggies/seed', async (req, res) => {
   try {
     await Veggie.create([
@@ -71,9 +118,12 @@ app.get('/veggies/seed', async (req, res) => {
     console.error(error)
     res.status(500).send("Error seeding data")
   }
-})
+});
 
-app.use('/veggies', Veggies)
+/*** Uae Routes: Where to Find Endpoint Data */
+app.use('/fruits', Fruits);
+app.use('/veggies', Veggies);
+
 
 /*** Error Handling ***/
 app.use((err, _req, res, next) => {
