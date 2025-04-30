513c45a460435353e0759826331a872af07f4017
Rainikka <rainikka@gmail.com>
2025-04-30 02:27:20 -0400

    routes to endpoints added

--- ProblemSets/ALAB319/SBA319/routes/routes.js
+++ ProblemSets/ALAB319/SBA319/routes/routes.js
@@ -1,34 +1,35 @@
-/*** Set: Routes for Endpoints ***/
+/*** Set: Router for Endpoints ***/
 const router = express.Router();
 const routes = require('./routes/routes');
-module.exports = router;
 app.use('./api', routes);
 
 
-/***** Endopoint Route: Post *******/
+/****** Endopoint Routes: INDUCES *********/
 
 /*** Endpoint: Post Method ***/
 router.post('/post', (req, res) => {
-  res.send('Post API')
+  res.send('Add New Post')
+});
+
+/*** Endpoint: Delete Method By ID ***/
+router.delete('/delete/:id', (req, res) => {
+  res.send('Delete Item By Id')
+});
+
+/*** Endpoint: Update By ID Method ***/
+router.put('/update/:id', (req, res) => {
+  res.send('Update Item By ID')
 });
 
 /*** Endpoint: Get All Method ***/
 router.get('/getAll', (req, res) => {
-  res.send('Get All API')
+  res.send('Get Collection of All Items')
 });
 
-/*** Endpoint: Get By Id Method ***/
+/*** Endpoint: Get One By ID Method ***/
 router.get('/getOne/:id', (req, res) => {
-  res.send('Get By ID API')
+  res.send('Get Item By ID')
 });
 
-/*** Endpoint: Update By Id Method ***/
-router.post('/post', (req, res) => {
-  res.send('Post API')
-});
 
-/*** Endpoint: Delete Method Method ***/
-router.delete('/post', (req, res) => {
-  res.send('Post API')
-});
-
+module.exports = router;
