78d9ad938cf6a50f78b9c1ac6bdec5e64dc7822b
Rainikka <rainikka@gmail.com>
2025-05-06 17:19:37 -0400

    webpage converted

--- ProblemSets/ALAB320/alab320-02/src/FashionBlog.js
+++ ProblemSets/ALAB320/alab320-02/src/FashionBlog.js
@@ -26,15 +26,24 @@ function FashionBlog() {
 
       {/* Main Content */}
       <main className="main">
-        <div className="article-dates article-date-1">11/11/20</div>
-        <div className="article-titles article-title-1">On the Street in Brooklyn</div>
-        <img className="article-images image1" src={sartre1} alt="Brooklyn street fashion" />
-        <article className="articles article1">Quisque a auctor neque. Aliquam erat volutpat. Praesent tristique malesuada leo at commodo. Nullam ut accumsan ligula, et hendrerit ex. Fusce est massa, placerat quis elit sed, varius gravida tellus. Mauris a purus lobortis, posuere massa ut, blandit tellus. In accumsan nisi vitae iaculis ultrices. In lectus libero, scelerisque vitae interdum placerat, tempor ac nunc. Mauris cursus urna dolor, ac porta nibh interdum nec. Aliquam placerat erat vitae luctus sollicitudin.</article>
+        <section classname="article-container">
+          <div className="article-dates article-date-1">11/11/20</div>
+          <div className="article-titles article-title-1">On the Street in Brooklyn</div>
+          <img className="article-images image1" src={sartre1} alt="Brooklyn street fashion" />
+          <article className="articles article1">
+            <p className="article-texts"> Quisque a auctor neque. Aliquam erat volutpat. Praesent tristique malesuada leo at commodo. Nullam ut accumsan ligula, et hendrerit ex. Fusce est massa, placerat quis elit sed, varius gravida tellus. Mauris a purus lobortis, posuere massa ut, blandit tellus. In accumsan nisi vitae iaculis ultrices. In lectus libero, scelerisque vitae interdum placerat, tempor ac nunc. Mauris cursus urna dolor, ac porta nibh interdum nec. Aliquam placerat erat vitae luctus sollicitudin.
+            </p> </article>
+        </section>
 
-        <div className="article-dates article-date-1">11/12/20</div>
-        <div className="article-titles article-title-1">Vintage in Vogue</div>
-        <img className="article-images image1" src={sartre2} alt="Vintage fashion" />
-        <article className="articles article1">Nam eleifend urna libero, vel aliquam lacus dictum in. Mauris bibendum convallis libero, in consequat sapien dignissim a. Nunc faucibus magna at lorem porttitor, vel elementum velit sagittis. Donec porttitor et mi non placerat. Donec rutrum quis ex convallis venenatis. Aliquam in hendrerit odio. Aenean dolor enim, laoreet quis ipsum ac, imperdiet blandit lectus. Vestibulum cursus mauris id nulla semper scelerisque. Fusce metus nisi, vehicula in tristique quis, molestie quis sem. Morbi ut finibus ipsum, ac finibus sapien.</article>
+
+        <section classname="article-container">
+          <div className="article-dates article-date-1">11/12/20</div>
+          <div className="article-titles article-title-1">Vintage in Vogue</div>
+          <img className="article-images image1" src={sartre2} alt="Vintage fashion" />
+          <article className="articles article1">
+            <p className="article-texts"> Nam eleifend urna libero, vel aliquam lacus dictum in. Mauris bibendum convallis libero, in consequat sapien dignissim a. Nunc faucibus magna at lorem porttitor, vel elementum velit sagittis. Donec porttitor et mi non placerat. Donec rutrum quis ex convallis venenatis. Aliquam in hendrerit odio. Aenean dolor enim, laoreet quis ipsum ac, imperdiet blandit lectus. Vestibulum cursus mauris id nulla semper scelerisque. Fusce metus nisi, vehicula in tristique quis, molestie quis sem. Morbi ut finibus ipsum, ac finibus sapien.</p>
+          </article>
+        </section>
       </main>
 
       {/* Footer */}
