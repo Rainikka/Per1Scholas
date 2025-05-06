84bea41af74d5dc04d5834a2229d6b25c603e856
Rainikka <rainikka@gmail.com>
2025-05-06 06:55:45 -0400

    html and css added

--- ProblemSets/ALAB320/alab320-1/src/layoutPage.js
+++ ProblemSets/ALAB320/alab320-1/src/layoutPage.js
@@ -1,34 +1,33 @@
-ddidimport React from 'react';
+import React from 'react';
 import './layoutPage.css';
 
-function layoutPage() {
+function LayoutPage() {
   return (
-    <div calssName="page-container">
-      { /* Left Column */}
+    <div className="page-container">
+      {/* Left Column */}
       <div className="left-column">
-        { /*Top box split in two*/}
-        <div className="top-split-container">
-          <div className="split-box-left-split"></div>
-          <div className="split-box-right-split"></div>
+        {/* Top box split in two */}
+        <div className="left-top-box">
+          <div className="left-top-box1"></div>
+          <div className="left-top-box2"></div>
         </div>
 
-        { /* 6 more equal boxes */}
+        {/* 6 more equal boxes */}
         <div className="box"></div>
         <div className="box"></div>
         <div className="box"></div>
         <div className="box"></div>
         <div className="box"></div>
         <div className="box"></div>
-      </div>
-      { /* Right Column */}
-      <div className="right-column">
+      </div >
+
+      {/* Right Column */}
+      < div className="right-column" >
         <div className="right-top-box"></div>
         <div className="right-bottom-box"></div>
-
-      </div>
-
+      </ >
     </div >
   );
 }
 
-export defautl layoutPage
+export default LayoutPage;
