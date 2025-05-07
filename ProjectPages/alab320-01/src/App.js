e8060d8cde5c82b9e3622ff8ebc065e34ae03cf8
Rainikka <rainikka@gmail.com>
2025-05-06 20:57:29 -0400

    ready for submission

---
+++ ProjectPages/alab320-01/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LayoutPage from './LayoutPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ ProjectPages/alab320-02/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FashionBlog from './FashionBlog'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FashionBlog />
+    </div>
+  );
+}
+
+export default App;
