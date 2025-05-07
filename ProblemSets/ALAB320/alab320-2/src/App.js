4e40af370cef1c7391f6006082a8f7596470ea92
Rainikka <rainikka@gmail.com>
2025-05-07 06:11:28 -0400

    first React app completed

--- ProblemSets/ALAB320/alab320-2/src/App.js
+++ ProblemSets/ALAB320/alab320-2/src/App.js
@@ -2,17 +2,23 @@ function Header() {
   return (<h1> Simple React Application </h1>)
 }
 
-function Content() {
-  return (<h1> Created by me, of course. </h1>)
+function Content(props) {
+  return (<p style={{ color: props.color }}>{props.text}</p>)
 }
 
 function Footer() {
-  return (<p> This is my first  React Application</p>)
+  return (<h1> Created by Me, of course, Woohoo! </h1>)
 }
 
 function App() {
   return (
-    <p> Filling in the blanks.</p>
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React application" />
+      <Content color="red" text="Wish me luck" />
+      <Content color="green" text="I think Ive got it" />
+      <Footer />
+    </>
   );
 }
 
