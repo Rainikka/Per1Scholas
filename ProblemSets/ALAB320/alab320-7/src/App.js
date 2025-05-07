a45299bf503e6a2c808571b22c5c64a12cfac643
Rainikka <rainikka@gmail.com>
2025-05-07 17:02:11 -0400

    movie api display completed like magic

--- ProblemSets/ALAB320/alab320-7/src/App.js
+++ ProblemSets/ALAB320/alab320-7/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import {
+  useState, useEffect
+} from 'react';
 import './App.css';
 
-function App() {
+// Import our components 
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  // Constant with your API key
+  const apiKey = "98e3fb1f";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get moview
+  const getMovie = async (searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the movie state to the received data
+    setMovie(data);
+  };
+
+  // This will run on the first render but not on subsequent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
 
-export default App;
