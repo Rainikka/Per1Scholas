9a6ebe8b9e717d7afce10fdbd7087b88cdf4ed79
Rainikka <rainikka@gmail.com>
2025-05-15 14:56:27 -0400

    form tweaked

--- ProjectPages/rainikka-capstone-app/backend/models/school.model.js
+++ ProjectPages/rainikka-capstone-app/backend/models/school.model.js
@@ -28,7 +28,6 @@ const SchoolSchema = new mongoose.Schema({
     type: String,
     required: [true, "Enter the school name"]
   },
-
   StreetAddress: {
     type: String,
     required: [true, "Enter the school's street address"]
@@ -39,8 +38,9 @@ const SchoolSchema = new mongoose.Schema({
   },
   SchoolNeighborhood: {
     type: String,
-    required: [true, "Enter the school neighborhood"]
+    required: [false, "Enter the school neighborhood"]
   },
+
   SchoolDistrict: {
     type: Number,
     required: [true, "Enter school district number"]
@@ -50,7 +50,6 @@ const SchoolSchema = new mongoose.Schema({
     required: [true, "Select school borough"],
     enum: ['Manhattan', 'Brooklyn', 'Queens', 'Bronx', 'Staten Island']
   },
-
   SchoolType: {
     type: String,
     required: [true, "Select school type"],
@@ -68,24 +67,22 @@ const SchoolSchema = new mongoose.Schema({
   },
   CommunityBasedOrg: {
     type: String,
-    required: [true, "Enter the community-based partner for the school"]
+    required: [false, "Enter the community-based partner for the school"]
   },
   PartnershipYearStart: {
     type: Number,
-    required: true,
-    enum: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+    required: [false, "Select the year the community  partnership started"],
+    enum: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]
   },
   Enrollment: {
     type: Number,
     required: [true, "Enter total number of students enrolled in school"]
   },
-
   AddDataForSchYear: {
     type: Number,
     required: [true, "Select the school year for the school data"],
-    enum: [2018, 2019, 2020, 2021, 2022, 2023, 2024]
+    enum: [2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]
   },
-
   /*** Student Demographics ***/
   FemaleStudents: {
     type: Number,
@@ -141,180 +138,209 @@ const SchoolSchema = new mongoose.Schema({
   /*** 2028 - 2024 School Attendance ***/
   Attendance2018Rate: {
     type: Number,
-    required: [true, "Enter the school attendance rate for 2018"]
+    required: [false, "Enter the school attendance rate for 2018"]
   },
   Attendance2019Rate: {
     type: Number,
-    required: [true, "Enter the school attendance rate for 2019"]
+    required: [false, "Enter the school attendance rate for 2019"]
   },
   Attendance2020Rate: {
     type: Number,
-    required: [true, "Enter the school attendance rate for 2020"]
+    required: [false, "Enter the school attendance rate for 2020"]
   },
 
   Attendance2021Rate: {
     type: Number,
-    required: [true, "Enter the school attendance rate for 2021"]
+    required: [false, "Enter the school attendance rate for 2021"]
   },
+
   Attendance2022Rate: {
     type: Number,
-    required: [true, "Enter the school attendance rate for 2022"]
+    required: [false, "Enter the school attendance rate for 2022"]
   },
   Attendance2023Rate: {
     type: Number,
-    required: [true, "Enter the school attendance rate for 2023"]
+    required: [false, "Enter the school attendance rate for 2023"]
   },
   Attendance2024Rate: {
     type: Number,
-    required: [true, "Enter the school attendance rate for 2024"]
+    required: [false, "Enter the school attendance rate for 2024"]
+  },
+  Attendance2025Rate: {
+    type: Number,
+    required: [false, "Enter the school attendance rate for 2025"]
   },
 
   Chronically2018Absent: {
     type: Number,
-    required: [true, "Enter the percentage of students chronically absent for 2018"]
+    required: [false, "Enter the percentage of students chronically absent for 2018"]
   },
   Chronically2019Absent: {
     type: Number,
-    required: [true, "Enter the percentage of students chronically absent for 2019"]
+    required: [false, "Enter the percentage of students chronically absent for 2019"]
   },
   Chronically2020Absent: {
     type: Number,
-    required: [true, "Enter the percentage of students chronically absent for 2021"]
+    required: [false, "Enter the percentage of students chronically absent for 2021"]
   },
   Chronically2021Absent: {
     type: Number,
-    required: [true, "Enter the percentage of students chronically absent for 2021"]
+    required: [false, "Enter the percentage of students chronically absent for 2021"]
   },
   Chronically2022Absent: {
     type: Number,
-    required: [true, "Enter the percentage of students chronically absent for 2022"]
+    required: [false, "Enter the percentage of students chronically absent for 2022"]
   },
   Chronically2023Absent: {
     type: Number,
-    required: [true, "Enter the percentage of students chronically absent for 2023"]
+    required: [false, "Enter the percentage of students chronically absent for 2023"]
   },
   Chronically2024Absent: {
     type: Number,
-    required: [true, "Enter the percentage of students chronically absent for 2024"]
+    required: [false, "Enter the percentage of students chronically absent for 2024"]
+  },
+  Chronically2025Absent: {
+    type: Number,
+    required: [false, "Enter the percentage of students chronically absent for 2025"]
   },
 
   /*** 2028 - 2024 ELA Scores ***/
   Avg2018ELAScore: {
     type: Number,
-    required: [true, "Enter the average score for English Language Arts in 2018"]
+    required: [false, "Enter the average score for English Language Arts in 2018"]
   },
   Avg2019ELAScore: {
     type: Number,
-    required: [true, "Enter the average score for English Language Arts in 2019"]
+    required: [false, "Enter the average score for English Language Arts in 2019"]
   },
   Avg2020ELAScore: {
     type: Number,
-    required: [true, "Enter the average score for English Language Arts in 2020"]
+    required: [false, "Enter the average score for English Language Arts in 2020"]
   },
   Avg2021ELAScore: {
     type: Number,
-    required: [true, "Enter the average score for English Language Arts in 2021"]
+    required: [false, "Enter the average score for English Language Arts in 2021"]
   },
   Avg2022ELAScore: {
     type: Number,
-    required: [true, "Enter the average score for English Language Arts in 2022"]
+    required: [false, "Enter the average score for English Language Arts in 2022"]
   },
   Avg2023ELAScore: {
     type: Number,
-    required: [true, "Enter the average score for English Language Arts in 2023"]
+    required: [false, "Enter the average score for English Language Arts in 2023"]
   },
   Avg2024ELAScore: {
     type: Number,
-    required: [true, "Enter the average score for English Language Arts in 2024"]
+    required: [false, "Enter the average score for English Language Arts in 2024"]
   },
-
-
+  Avg2025ELAScore: {
+    type: Number,
+    required: [false, "Enter the average score for English Language Arts in 2025"]
+  },
   Proficient2018ELA: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for ELA in 2018"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2018"]
   },
   Proficient2019ELA: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for ELA in 2019"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2019"]
   },
   Proficient2020ELA: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for ELA in 2020"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2020"]
   },
   Proficient2021ELA: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for ELA in 2021"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2021"]
   },
   Proficient2022ELA: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for ELA in 2022"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2022"]
   },
   Proficient2023ELA: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for ELA in 2023"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2023"]
   },
   Proficient2024ELA: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for ELA in 2024"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2024"]
   },
-
+  Proficient2025ELA: {
+    type: Number,
+    required: [false, "Enter the percentage of student scores at or above the proficient level for ELA in 2025"]
+  },
   /*** 2028 - 2024 Math Scores ***/
   Avg2018MathScore: {
     type: Number,
-    required: [true, "Enter the average score for Math in 2018"]
+    required: [false, "Enter the average score for Math in 2018"]
   },
   Avg2019MathScore: {
     type: Number,
-    required: [true, "Enter the average score for Math in 2019"]
+    required: [false, "Enter the average score for Math in 2019"]
   },
   Avg2020MathScore: {
     type: Number,
-    required: [true, "Enter the average score for Math in 2020"]
+    required: [false, "Enter the average score for Math in 2020"]
   },
   Avg2021MathScore: {
     type: Number,
-    required: [true, "Enter the average score for Math in 2021"]
+    required: [false, "Enter the average score for Math in 2021"]
   },
   Avg2022MathScore: {
     type: Number,
-    required: [true, "Enter the average score for Math in 2022"]
+    required: [false, "Enter the average score for Math in 2022"]
   },
   Avg2023MathScore: {
     type: Number,
-    required: [true, "Enter the average score for Math in 2023"]
+    required: [false, "Enter the average score for Math in 2023"]
   },
   Avg2024MathScore: {
     type: Number,
-    required: [true, "Enter the average score for Math in 2024"]
+    required: [false, "Enter the average score for Math in 2024"]
   },
-
+  Avg2025MathScore: {
+    type: Number,
+    required: [false, "Enter the average score for Math in 2025"]
+  },
   Proficient2018Math: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for Math in 2018"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for Math in 2018"]
   },
   Proficient2019Math: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for Math in 2019"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for Math in 2019"]
   },
   Proficient2020Math: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for Math in 2020"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for Math in 2020"]
   },
   Proficient2021Math: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for Math in 2021"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for Math in 2021"]
   },
   Proficient2022Math: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for Math in 2022"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for Math in 2022"]
   },
   Proficient2023Math: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for Math in 2023"]
+    required: [false, "Enter the percentage of students, who scored at or above the proficient level for Math in 2023"]
   },
   Proficient2024Math: {
     type: Number,
-    required: [true, "Enter the percentage of students, who scored at or above the proficient level for Math in 2024"]
+    required: [false, "Enter the percentage of student scores at or above the proficient level for Math in 2024"]
+  },
+  Proficient2025Math: {
+    type: Number,
+    required: [false, "Enter the percentage of student scores at or above the proficient level for Math in 2025"]
+  },
+  SchoolLatitude: {
+    type: Number,
+    required: [false, "Enter the school's latitudinal coordinates"]
+  },
+  SchoolLongitude: {
+    type: Number,
+    required: [false, "Enter the school's longitudinal coordinates"]
   }
 
 }, {
