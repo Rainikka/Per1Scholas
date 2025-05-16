bd332c35c661ac7195d1f313dbc2bba171493e0c
Rainikka <rainikka@gmail.com>
2025-05-16 03:51:27 -0400

    checking authorizaton

---
+++ ProjectPages/rainikka-capstone-app/backend/models/admin.model.js
@@ -0,0 +1,16 @@
+const adminSchema = new mongoose.Schema({
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    validate: {
+      validator: function(v) {
+        return v.endsWith('@admin.nyc.schools.gov');
+      },
+      message: props => `${props.value} is not a valid admin email!`
+    }
+  },
+  password: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
